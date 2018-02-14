ac8c24c1d00fcb858b34b6dcb67ffd5506861554
Ray <ray@protozo.net>
2018-02-14 02:38:13 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,30 @@
+const app = "I don't do much."
+
+function destructivelyAppendKitten(name)
+{
+  kittens.push(name);
+}
+
+function destructivelyPrependKitten(name)
+{
+  kittens.unshift(name);
+}
+
+function destructivelyRemoveLastKitten()
+{
+  kittens.pop();
+}
+
+function destructivelyRemoveFirstKitten()
+{
+  kittens.shift();
+}
+
+function appendKitten(name)
+{
+  
+  var newkitten = kittens;
+  
+  return [newkitten, ...name];
+
+}
